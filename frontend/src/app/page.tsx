882ca64c69e9637af75d5e0ae42a4d9d129e2b08
Angan Samadder <rio.angan@gmail.com>
2024-09-29 11:01:49 -0500

    change tickers and add title to homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,11 +10,13 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-evenly h-screen px-8 pt-8 font-[family-name:var(--font-geist-sans)]">
       {user ? (
-        <main className="flex flex-col gap-8 py-16 justify-center items-center overflow-hidden">
-          <div className="justify-center py-16 space-x-4">
-            <CompanyButton company={"Apple"} />
-            <CompanyButton company={"Alphabet"} />
-            <CompanyButton company={"Meta"} />
+        <main className="flex flex-col gap-8 py-8 justify-center items-center">
+          <div className="pt-8">
+            <TickerComponent></TickerComponent>
+          </div>
+          <h1>Hello, {user.name}!</h1>
+          <div className="h-[30rem] flex items-center justify-center">
+            <TextHoverEffect text="Hello, User!" />
           </div>
           <SearchBar placeholder="Ask a Question" className="w-1/2 h-16" />
         </main>
@@ -39,7 +41,6 @@ export default function Home() {
           </button>
         </div>
       )}
-      <NewsFeed />
     </div>
   );
 }
